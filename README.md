18d8c999acc03caffd04ce79f3fc26881e551b71
Jesse Vahlfors <127712759+JesseVahlfors@users.noreply.github.com>
2023-05-02 22:33:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Mixed Messages
+
+## The Plan
+
+### The Function
+- I am making a message generator that outputs a random 3 part workout plan. Each workout has a Push, Pull, Hip-hinge and Squat Movement.
+
+### Built with
+- Visual studio code
+- Javascript
+- git and github
+- git bash command line
+
+### Timeline
+- Deadline 23:59 Saturday 6th of May 2023
---
+++ main.js
@@ -0,0 +1,6 @@
+const workout = {
+    push: [ "bench press", "overhead press", "push-ups", "machine chest press"],
+    pull: ["pull-ups", "lat pulldown", "barbell row", "dumbbell row", "cable row"],
+    hip_hinge: ["deadlift", "good morning", "Romanian deadlift", "clean", "kettlebell swing"],
+    squat: ["back squat", "front squat", "Bulgarian split squat", "goblet squat", "leg press"]
+}
